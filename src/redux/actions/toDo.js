397c76345e369dd45a7072873b3b444cb85d3a3e
Dan Day <danday@gmail.com>
2020-03-07 14:07:33 -0600

    sockets working!

--- src/redux/actions/toDo.js
+++ src/redux/actions/toDo.js
@@ -1,32 +1,32 @@
-// import socket from "../socket"
-import {Socket} from "phoenix"
-let socket = new Socket("ws://localhost:4000/socket", {params: {token: window.userToken}})
-socket.connect()
-
-// Now that you are connected, you can join channels with a topic:
-let channel           = socket.channel("room:lobby", {})
-// let chatInput         = document.querySelector("#chat-input")
-// let messagesContainer = document.querySelector("#messages")
-
-// chatInput.addEventListener("keypress", event => {
-//     if(event.keyCode === 13){
-//         channel.push("new_msg", {body: chatInput.value})
-//         chatInput.value = ""
-//     }
+// // import socket from "../socket"
+// import {Socket} from "phoenix"
+// let socket = new Socket("ws://localhost:4000/socket", {params: {token: window.userToken}})
+// socket.connect()
+//
+// // Now that you are connected, you can join channels with a topic:
+// let channel           = socket.channel("room:lobby", {})
+// // let chatInput         = document.querySelector("#chat-input")
+// // let messagesContainer = document.querySelector("#messages")
+//
+// // chatInput.addEventListener("keypress", event => {
+// //     if(event.keyCode === 13){
+// //         channel.push("new_msg", {body: chatInput.value})
+// //         chatInput.value = ""
+// //     }
+// // })
+//
+// channel.on("new_msg", payload => {
+//     console.log('new msg', payload)
+//     // let messageItem = document.createElement("li")
+//     // messageItem.innerText = `[${Date()}] ${payload.body}`
+//     // messagesContainer.appendChild(messageItem)
 // })
-
-channel.on("new_msg", payload => {
-    console.log('new msg', payload)
-    // let messageItem = document.createElement("li")
-    // messageItem.innerText = `[${Date()}] ${payload.body}`
-    // messagesContainer.appendChild(messageItem)
-})
-
-
-
-channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+//
+//
+//
+// channel.join()
+//     .receive("ok", resp => { console.log("Joined successfully", resp) })
+//     .receive("error", resp => { console.log("Unable to join", resp) })
 
 
 export const addTodoRequest = text => {
@@ -47,9 +47,46 @@ export const receiveToDo = payload => dispatch => {
     dispatch(addTodoSuccess(payload))
 }
 
+export const dispatchMsg = msg => dispatch => {
+console.log('dispatch msg!!!', msg)
+    dispatch({
+        type: msg.body.type,
+        payload: msg.body.payload,
+        audit: msg.body.audit,
+        server_msg: true
+    });
+    // channel.push("new_msg", {body: text})
+
+    // channel.push('new:todo', payload)
+    //     .receive('ok', response => {
+    //         console.log('created TODO', response);
+    //         // dispatch(addTodoSuccess(text));
+    //     })
+    //     .receive('error', error => {
+    //         console.error(error);
+    //         dispatch(addTodoFailure(text, error));
+    //     });
+
+    // dispatch(addTodoSuccess(text));
+    // dispatch(addTodoFailure(text));
+    // dispatch({
+    //     type: "ADD_TODO_REQUEST",
+    //     payload: text
+    // });
+    // dispatch({
+    //     type: "ADD_TODO_SUCCESS",
+    //     payload: text
+    // });
+    // dispatch({
+    //     type: "ADD_TODO_SUCCESS",
+    //     payload: text
+    // });
+};
+
+
 
 export const addTodo = text => dispatch => {
-    channel.push("new_msg", {body: text})
+    // channel.push("new_msg", {body: text})
 
     // channel.push('new:todo', payload)
     //     .receive('ok', response => {
