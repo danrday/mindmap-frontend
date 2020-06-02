a3b74aa0182603d0cda4bf3eabb80186839792b3
Dan Day <danday@gmail.com>
2020-06-02 16:56:25 -0500

    generally working

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,7 @@ import NavAndHeader from "./NavAndHeader";
 import Header from "./Header";
 import Nav from "./Nav";
 import NavItems from "./NavItems";
+import PageOptions from "./PageOptions";
 import styled from "styled-components";
 import theme from "./styles/theme";
 
@@ -41,6 +42,14 @@ class Page extends Component {
             channel={this.props.channel}
           />
 
+          <PageOptions
+            navIsOpen={this.state.navIsOpen}
+            navIsHovered={this.state.navIsHovered}
+            toggle={this.handleToggleNav}
+            hover={this.handleHoverNav}
+            channel={this.props.channel}
+          />
+
           {this.props.children}
         </MainFrame>
       </StyledPage>
@@ -59,6 +68,33 @@ const MainFrame = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
   }
+`;
+
+const SelectedMenuFrame = styled.div`
+  @media (max-width: 768px) {
+    top: 120px;
+    left: 0px;
+    height: 120px;
+    width: 100%;
+  }
+  position: fixed;
+  top: 60px;
+  left: 60px;
+  bottom: 0;
+  padding-top: 15px;
+  transition: all 0.2s ease-in-out;
+  overflow-x: hidden;
+  z-index: 50;
+  width: 0px;
+  background-color: purple;
+  ${({ openNav, hoverNav }) =>
+    (openNav || hoverNav) &&
+    `
+        left: 60px;
+        width: 200px;
+        background-color: #9bccff;
+         box-shadow: inset -4px 0px 2px -2px purple;
+      `}
 `;
 
 export default Page;
--- src/components/PageOptions.js
+++ src/components/PageOptions.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import navLinks from "./NavLinks";
 
+import Node from "./pages/node";
+
 import { connect } from "react-redux";
 import {
   document,
@@ -13,10 +15,21 @@ import {
 
 import { selectPage } from "../redux/actions/ui";
 
-class NavItems extends Component {
+class PageOptions extends Component {
+  state = {
+    selectedSubItem: null,
+    selectedMenu: null
+  };
   handleSelected(i) {
     this.props.selectPage(i, this.props.selectedPage);
   }
+  handleSelectedSubItem(i) {
+    this.setState({ selectedSubItem: i });
+  }
+  componentDidMount() {
+    this.props.openDocument();
+  }
+
   selectedMenu() {
     if (this.props.selectedPage) {
       return navLinks[this.props.selectedPage].component();
@@ -48,54 +61,16 @@ class NavItems extends Component {
 
   render() {
     return (
-      <NavItemsFrame
+      <SelectedMenuFrame
         openNav={this.props.navIsOpen}
         hoverNav={this.props.navIsHovered}
         onMouseEnter={() => this.props.hover(true)}
         onMouseLeave={() => this.props.hover(false)}
       >
-        {navLinks.map((item, i) => {
-          const isSelected = i === this.props.selectedPage;
-          const subItems = item.subItems;
-
-          const isNodeItem = item.link === "/node";
-
-          const isLocked = this.props.lockedPages.includes(i);
-
-          return (
-            <div key={item.link}>
-              <NavItem
-                disabled={isLocked}
-                onClick={() => {
-                  if (isLocked) {
-                    alert("Another user is editing the global settings.");
-                  } else {
-                    this.handleSelected(i);
-                  }
-                }}
-                isSelected={isSelected}
-              >
-                <div className="navIconFrame">
-                  <div className="navIcon">
-                    <i
-                      className={
-                        isNodeItem && this.props.currSelNode
-                          ? item.altClassName
-                          : item.className
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="navItemText">
-                  {isNodeItem && this.props.currSelNode
-                    ? item.altNavItemText
-                    : item.navItemText}
-                </div>
-              </NavItem>
-            </div>
-          );
-        })}
-      </NavItemsFrame>
+        <div style={{ width: "180px", height: "100%" }}>
+          {this.selectedMenu()}
+        </div>
+      </SelectedMenuFrame>
     );
   }
 
@@ -107,13 +82,109 @@ class NavItems extends Component {
   };
 }
 
-const NavItemsFrame = styled.div`
-  @media (max-width: 768px) {
+const StyledHeader = styled.div`
+  height: 60px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 1030;
+  background-color: #312a48;
+  box-shadow: 0 1px 4px 0px rgba(0, 0, 0, 0.16);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  transition: all 0.2s ease-in-out;
+  ${({ openNav }) =>
+    openNav &&
+    `
+        // left: 170px;
+        // background: lightgrey;
+      `}
+  .projectTitle {
+    font-family: "Poppins", "Helvetica Neue", Arial, sans-serif;
+    font-size: 2rem;
+    margin-right: 20px;
+    margin-bottom: 0px;
+    letter-spacing: -1px;
+  }
+  .iconFrame {
+    .close {
+      position: absolute;
+      opacity: 0;
+    }
+    .hamburger {
+      opacity: 1;
+    }
+    transition: all 0.2s ease-in-out;
+    ${({ openNav }) =>
+      openNav &&
+      `       
+              .close {
+                  position: relative;
+                  opacity: 1;
+              }
+              .hamburger {
+                  opacity: 0;
+                  position: absolute;
+              }
+            -webkit-transform: rotate(90deg);
+            -moz-transform: rotate(90deg);
+            -ms-transform: rotate(90deg);
+            -o-transform: rotate(90deg);
+            transform: rotate(90deg);
+          `}
+  }
+  .hamburgerFrame {
+    width: 60px;
+    height: 60px;
+    border-right: 1px solid rgba(0, 0, 0, 0.15);
     display: flex;
-    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    color: #868e96;
+    font-size: 20px;
+    transition: all 0.2s ease-in-out;
+    ${({ openNav }) =>
+      openNav &&
+      `
+            margin-left: 200px;
+            background-color: #584586;
+
+          `}
+    &:hover {
+      background-color: #e5fdff;
+    }
+  }
+`;
+
+const SelectedMenuFrame = styled.div`
+  @media (max-width: 768px) {
+    top: 120px;
+    left: 0px;
+    height: 120px;
     width: 100%;
   }
-  flex-direction: column;
+  padding-top: 15px;
+  transition: all 0.2s ease-in-out;
+  overflow-x: hidden;
+  z-index: 50;
+  width: 0px;
+  background-color: purple;
+  ${({ openNav, hoverNav }) =>
+    (openNav || hoverNav) &&
+    `
+        width: 200px;
+        background-color: #9bccff;
+         box-shadow: inset -4px 0px 2px -2px purple;
+      `}
+`;
+
+const NavBar = styled.div`
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+  position: fixed;
   top: 60px;
   left: 0px;
   bottom: 0;
@@ -125,8 +196,18 @@ const NavItemsFrame = styled.div`
   ${({ openNav, hoverNav }) =>
     (openNav || hoverNav) &&
     `
-        background: yellow;
+        left: 0px;
+        width: 180px;
+        background: #65bbd8;
       `}
+`;
+
+const NavItemsFrame = styled.div`
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: row;
+  }
+  flex-direction: column;
 `;
 
 const NavItem = styled.div`
@@ -192,39 +273,4 @@ const mapDispatchToProps = (dispatch, props) => ({
   selectPage: (pageName, currentPage) =>
     dispatch(selectPage(pageName, currentPage))
 });
-export default connect(mapStateToProps, mapDispatchToProps)(NavItems);
-
-// {isSelected &&
-// subItems &&
-// subItems.map((item, i) => {
-//     const isSelectedSubItem = i === this.state.selectedSubItem;
-//
-//     const isNodeItem = item.link === "/node";
-//     console.log("refiring?", item.link);
-//     return (
-//         <NavItem
-//             onClick={() => {
-//                 this.handleSelectedSubItem(i);
-//             }}
-//             isSelected={isSelectedSubItem}
-//             isSubItem={true}
-//         >
-//             <div className="navIconFrame">
-//                 <div className="navIcon">
-//                     <i
-//                         className={
-//                             !isNodeItem ? item.className : item.altClassName
-//                         }
-//                     />
-//                 </div>
-//             </div>
-//             <div className="navItemText">
-//                 {!isNodeItem ? item.navItemText : item.altNavItemText}
-//             </div>
-//         </NavItem>
-//     );
-// })}
-
-// handleSelectedSubItem(i) {
-//     this.setState({ selectedSubItem: i });
-// }
+export default connect(mapStateToProps, mapDispatchToProps)(PageOptions);
